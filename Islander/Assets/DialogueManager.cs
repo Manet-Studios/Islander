64edefc0daec31b647bcf3627c1f25cf074147c9
Richard Palmer <ricky@rpgrafx.com>
2018-09-18 21:16:08 -0400

    Initial commit

---
+++ Islander/Assets/DialogueManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour
+{
+    [SerializeField]
+    private Image thumbnailIcon;
+
+    [SerializeField]
+    private Text dialogueText;
+
+    [SerializeField]
+    private Button[] optionButtons;
+
+    private bool waitingToContinue;
+
+    private DialogueOption nextOption;
+
+    public void StartDialogue(Sprite thumbnail, bool playerOption, string text, IEnumerable<DialogueOption> nextOptions)
+    {
+        waitingToContinue = !playerOption;
+        if (playerOption == false)
+        {
+        }
+    }
+
+    private void Update()
+    {
+        if (waitingToContinue && Input.anyKey)
+        {
+        }
+    }
+}
---
+++ Islander/Assets/Dialoguer.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+[RequireComponent(typeof(Collider2D))]
+public class Dialoguer : MonoBehaviour, IInteractable
+{
+    public event Action<Sprite, bool, string, IEnumerable<DialogueOption>> ChooseDialogueOptionEventHandler;
+
+    [SerializeField]
+    private List<DialogueOption> dialogueOptions;
+
+    public int relationshipScore;
+    private DialogueManager dm;
+
+    private void Start()
+    {
+        dm = FindObjectOfType<DialogueManager>();
+        ChooseDialogueOptionEventHandler += dm.StartDialogue;
+    }
+
+    private void OnDestroy()
+    {
+        ChooseDialogueOptionEventHandler = null;
+    }
+
+    public void Interact()
+    {
+        if (ChooseDialogueOptionEventHandler != null)
+        {
+            DialogueOption availableOption = dialogueOptions.Where(d => relationshipScore < d.relationshipRange.y && relationshipScore >= d.relationshipRange.x).FirstOrDefault();
+            if (availableOption != null)
+            {
+                ChooseDialogueOptionEventHandler(availableOption.thumbnail, availableOption.playerOption, availableOption.dialogueText, availableOption.dialogueOptions.Where(d => relationshipScore < d.relationshipRange.y && relationshipScore >= d.relationshipRange.x));
+            }
+        }
+    }
+
+    public void ChooseDialogueOption(DialogueOption option)
+    {
+        relationshipScore += option.relationshipReward;
+
+        if (ChooseDialogueOptionEventHandler != null)
+        {
+            ChooseDialogueOptionEventHandler(option.thumbnail, option.playerOption, option.dialogueText, option.dialogueOptions.Where(d => relationshipScore < d.relationshipRange.y && relationshipScore >= d.relationshipRange.x));
+        }
+    }
+}
+
+public interface IInteractable
+{
+    void Interact();
+}
+
+[System.Serializable]
+public class DialogueOption
+{
+    public Sprite thumbnail;
+    public bool playerOption;
+
+    public Vector2Int relationshipRange;
+
+    public string dialogueText;
+
+    public int relationshipReward;
+
+    public DialogueOption[] dialogueOptions;
+}
